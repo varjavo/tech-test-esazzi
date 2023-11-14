f29edd1fb08c1a6b121537ab7c73ff206f29820f
Fernando Olvera <frnndolvr@gmail.com>
2023-11-14 12:18:55 -0600

    infinite-scroll implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,28 @@ export class AppComponent {
   cardsList: any[] = [];
 
   constructor() {
-    for (let index = 0; index < 200; index++) {
+    for (let index = 0; index < 10; index++) {
       // ! Fill this random list in order to test endless scroll package
       this.cardsList.push({
-        id: crypto.randomUUID(),
+        id: Math.random(),
         creation: new Date().toISOString(),
       });
     }
+  }
+
+  onBottomScrollReached() {
+    for (let index = 0; index < 10; index++) {
+      // ! Fill this random list in order to test endless scroll package
+      this.cardsList.push({
+        id: Math.random(),
+        creation: new Date().toISOString(),
+      });
+    }
+    console.log(
+      `Triggered at ${(
+        (100 * window.scrollY) /
+        document.body.clientHeight
+      ).toFixed(1)}%`
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, InfiniteScrollModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
