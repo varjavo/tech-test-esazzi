64620318e88ec98bfd13a004e65a8382e2706e7f
Fernando Olvera <frnndolvr@gmail.com>
2023-11-14 15:53:33 -0600

    style: general card layout

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -21,6 +21,23 @@ export class ApiService {
         id: Math.random(),
         creation: new Date().toISOString(),
         // ! Add all the needed properties according to the view
+        image:
+          Math.random() >= 0.5
+            ? `https://dummyimage.com/600x400/000/808.jpg&text=Hello+world`
+            : null,
+        summary: "Idea summary from service",
+        creator: {
+          anonymous: true,
+          user: null,
+          rate: 4.5,
+        },
+        createdAt: new Date().toISOString(),
+        assignees: [],
+        workflow: {
+          id: 3,
+          name: "Idea review/PLM II",
+        },
+        score: null,
       });
     }
     return of(itemsList);
