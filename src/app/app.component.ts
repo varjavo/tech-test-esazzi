f2ca5d8e0fbc0213a18f2bc76f11e082dae0f40d
Fernando Olvera <frnndolvr@gmail.com>
2023-11-14 11:26:13 -0600

    random items array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
-
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  cardsList: any[] = [];
+
+  constructor() {
+    for (let index = 0; index < 200; index++) {
+      // ! Fill this random list in order to test endless scroll package
+      this.cardsList.push({
+        id: crypto.randomUUID(),
+        creation: new Date().toISOString(),
+      });
+    }
+  }
+}
